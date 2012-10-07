d49dc66a28949ad7b7f9907049c564f0d3126b6e
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2012-10-08 02:28:07 +0300

    Added removing http sessions.

--- new/HttpSession.cpp
+++ new/HttpSession.cpp
@@ -4,6 +4,8 @@
 HttpSession::HttpSession(QTcpSocket* socket): socket(socket) {
 	connect(socket, SIGNAL(readyRead()),
 			this, SLOT(onData()));
+	connect(socket, SIGNAL(disconnected()),
+			this, SLOT(deleteLater()));
 }
 
 void HttpSession::onData() {
